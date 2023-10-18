9eaf7d28dce6052a3af5d5c09852f2867302aac2
harshithreddy8899 <harshithreddy8@gmail.com>
2023-10-18 12:29:24 -0400

    first commit

---
+++ Mem_react/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import GiphySearch from './GiphySearch.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <GiphySearch maximumScore={0} />
+    </div>
+  );
+}
+
+export default App;
